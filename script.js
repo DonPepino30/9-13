5ca0d792841b34639cb7ad5e3a2bd6ffa870acfe
DonPepino30 <131565147+DonPepino30@users.noreply.github.com>
2024-09-01 16:43:27 -0500

    final una flor

--- script.js
+++ script.js
@@ -103,11 +103,11 @@ const propiedadAnimacionP6={
 const elementPetalo7 =document.querySelector(".petalo7");
 
 const animacionPetalo7=[
-    {transform: "rotateZ(0deg)",left:"32.7%",top:"12%", background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset: 0},
-    {transform: "rotateZ(90deg)",left:"52%",top:"23%",background: "linear-gradient(to top, #D15B01, #FDC300, #FDC300)",offset: .25},
-    {transform: "rotateZ(180deg)",left:"33.4%",top:"34%",background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset:.50},
-    {transform: "rotateZ(270deg)",left:"14%",top:"23%",background: "linear-gradient(to top, #D15B01, #FDC300, #FDC300)",offset: .75},
-    {transform: "rotateZ(360deg)",left:"32.7%",top:"12%",background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset: 1},
+    {transform: "rotateZ(0deg)",left:"33.4%",top:"34%", background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset: 0},
+    {transform: "rotateZ(90deg)",left:"14%",top:"23%",background: "linear-gradient(to top, #D15B01, #FDC300, #FDC300)",offset: .25},
+    {transform: "rotateZ(180deg)",left:"32.7%",top:"12%",background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset:.50},
+    {transform: "rotateZ(270deg)",left:"52%",top:"23%",background: "linear-gradient(to top, #D15B01, #FDC300, #FDC300)",offset: .75},
+    {transform: "rotateZ(360deg)",left:"33.4%",top:"34%",background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset: 1},
 ];
 
 const propiedadAnimacionP7={
@@ -116,3 +116,30 @@ const propiedadAnimacionP7={
 };
 
 const elementPetalo8 =document.querySelector(".petalo8");
+
+const animacionPetalo8=[
+    {transform: "rotateZ(0deg)",left:"14%",top:"23%", background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset: 0},
+    {transform: "rotateZ(90deg)",left:"32.7%",top:"12%",background: "linear-gradient(to top, #D15B01, #FDC300, #FDC300)",offset: .25},
+    {transform: "rotateZ(180deg)",left:"52%",top:"23%",background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset:.50},
+    {transform: "rotateZ(270deg)",left:"33.4%",top:"34%",background: "linear-gradient(to top, #D15B01, #FDC300, #FDC300)",offset: .75},
+    {transform: "rotateZ(360deg)",left:"14%",top:"23%",background: "linear-gradient(to top, #565656, #FEFDFB, #FEFDFB)",offset: 1},
+];
+
+const propiedadAnimacionP8={
+    duration:duracionAnimacion,
+    easing: "linear",
+};
+
+
+function girarGirasol(){
+    elementoPetalo1.animate(animacionPetalo1, propiedadAnimacionP1);
+    elementoPetalo2.animate(animacionPetalo2, propiedadAnimacionP2);
+    elementoPetalo3.animate(animacionPetalo3, propiedadAnimacionP3);
+    elementoPetalo4.animate(animacionPetalo4, propiedadAnimacionP4);
+    elementoPetalo5.animate(animacionPetalo5, propiedadAnimacionP5);
+    elementoPetalo6.animate(animacionPetalo6, propiedadAnimacionP6);
+    elementoPetalo7.animate(animacionPetalo7, propiedadAnimacionP7);
+    elementoPetalo8.animate(animacionPetalo8, propiedadAnimacionP8);
+}
+
+window.addEventListener("load", iniciar);
